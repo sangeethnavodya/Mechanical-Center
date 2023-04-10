a348f2f8e7332f28e9643f12f5a6405d44cb006e
sangeethnavodya <pererakasn.19@uom.lk>
2023-04-10 23:47:01 +0530

    shop created

--- src/Components/CreateShopForm.js
+++ src/Components/CreateShopForm.js
@@ -1,95 +1,56 @@
-import React, { Component, useState } from 'react'
-import './ComponentCss/signup.css'
-import axios from 'axios'
+import React, { useState } from 'react';
+import './ComponentCss/signup.css';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function CreateShopForm() {
-    const [formData, setFormData] = useState({
-        Email: "", Password: ""
-    });
-    const [image, setImage] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [file, setFile] = useState(null);
 
-    const handleImageChange = (event) => {
-        const selectedImage = event.target.files[0];
-        setImage(selectedImage);
-    };
-    const [selectedOption, setselectedOption] = useState('')
-    const navigate = useNavigate();
-    function handleInputChange(e) {
-        const name = e.target.name;
-        const value = e.target.value;
-        setFormData((prev) => {
-            return { ...prev, [name]: value }
-        })
-    }
-    function handleRadioButton(event) {
-        setselectedOption(event.target.value)
-    }
-    function handleUser(event) {
-        event.preventDefault();
-        if (selectedOption === 'Owner') {
-            axios.post('http://localhost:4000/register/login', formData)
-                .then(response => {
-                    console.log(response.data);
-                    if (response.data.message === 'owner')
-                        navigate('/ownerHome')
-                })
-                .catch(error => {
-                    console.log(error);
-                    // Handle error response
-                });
-            console.log(formData)
-        }
-        if (selectedOption === 'Admin') {
-            axios.post('http://localhost:4000/register/adminLogin', formData)
-                .then(response => {
-                    if (response.data.message === 'Login successful') {
-                        navigate('/adminHome')
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                    // Handle error response
-                });
-            console.log(formData)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-        }
-        if (selectedOption === 'Customer') {
-            axios.post('http://localhost:4000/register/customerLogin', formData)
-                .then(response => {
-                    console.log(response.data);
-                    // Handle success response
-                })
-                .catch(error => {
-                    console.log(error);
-                    // Handle error response
-                });
-            console.log(formData)
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    for (let key in formData) {
+      data.append(key, formData[key]);
     }
-    return (
-        <form className='main'>
-            <div className='topic'>
-                <h3>Create Your Shop</h3>
-            </div>
-            <div className='form'>
-                <div className='Email'>
-                    <label className='emaillabel'>Shop Name</label>
-                    <input type="email" name="Email" className="FormControl" placeholder="Enter Name" onChange={handleInputChange} />
-                </div>
-                <label htmlFor="image" className='emaillabel' >Select an image:</label>
-                <input
-                    type="file"
-                    id="image"
-                    name="image"
-                    accept="image/*"
-                    onChange={handleImageChange}
-                    className="FormControl"
-                />
-                <button className='signup' onClick={handleUser}>Create Shop</button>
-            </div>
-        </form>
-    )
+    data.append('image', file);
+
+    try {
+      const response = await axios.post('/shop/create', data);
+      console.log(response.data);
+      // display success message or redirect to success page
+    } catch (error) {
+      console.log(error);
+      // display error message or handle error
+    }
+    console.log(data);
+  };
+
+  return (
+    <form className='main'>
+      <div className='topic'>
+        <h3>Create Your Shop</h3>
+      </div>
+      <div className='form'>
+        <div className='Email'>
+          <label className='emaillabel'>Shop Name</label>
+          <input type="email" name="Email" className="FormControl" placeholder="Enter Name" onChange={handleChange} />
+        </div>
+        <label htmlFor="image" className='emaillabel' >Select an image:</label>
+        <input type="file" className='FormControl' onChange={handleFileChange} />
+        <button className='signup' onClick={handleSubmit}>Create Shop</button>
+      </div>
+    </form>
+  )
 }
+
 export default CreateShopForm;
