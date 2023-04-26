48d46e209090ae3fe2a19308a2e33034eb32c98a
sangeethnavodya <pererakasn.19@uom.lk>
2023-04-26 10:36:11 -0700

    update review

--- src/Components/CustomerReview/ExistingCustomerReview.js
+++ src/Components/CustomerReview/ExistingCustomerReview.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import j from "../../Assests/star.jpg"
+import UpdateView from "./UpdateView";
+import { Button } from "antd";
 
 function ExisitngCustomerReview() {
     const [review, setReview] = useState([]);
     const [isReview, setIsReview] = useState(false);
+    const [update,setUpdate]=useState(false)
 
     useEffect(() => {
         async function fetchReviews() {
@@ -18,23 +22,157 @@ function ExisitngCustomerReview() {
         }
         fetchReviews();
     }, []);
+
+    function handleDelete(r){
+        axios.delete('http://localhost:4000/create/'+r._id).then(s=>{
+            console.log(s)
+            window.location.reload()
+        })
+    }
+    function handleUpdate(r){
+        setUpdate(true)
+    }
+    function handleReviewUpdate() {
+        // Do something with the updated review data
     
+        setUpdate(false);
+        window.location.reload()
+      }
+
     const cardViews = review?.map((review, index) => (
         <div className="card-review" key={index}>
             <div className="card-container-review">
-                <h4 className="item-desc-review-name">{review.Customer_Name}</h4>
-                <h5 className="item-desc-review">{review.Review}</h5>
+                <div className="name-review">
+                    <h4 className="item-desc-review-name">{review.Customer_Name}</h4>
+                    <h5 className="item-desc-review">{review.Review}</h5>
+                </div>
+                <div className="star-review">
+                    {(review.star == 1) && <div>
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /></div>}
+                    {(review.star == 2) && <div>
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /></div>}
+                    {(review.star == 3) && <div>
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /></div>}
+                    {(review.star == 4) && <div>
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /></div>}
+
+                    {(review.star == 5) && <div>
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /></div>}
+                    {(review.star == 6) && <div>
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        />
+                        <img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /><img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /><img
+                            src={j}
+                            width="30px"
+                            height="30px"
+                        /></div>}
+
+                </div>
+                <div>
+                {(review.Customer_Name == localStorage.getItem('customer_name') && <div>
+                       {!update&& <button onClick={()=>handleUpdate(review)}>Update</button>}
+                        <button onClick={()=>handleDelete(review)} className="btnUpdate">Delete</button>
+                        </div>)}
+                </div>
+             
             </div>
+            {update&&review.Customer_Name==localStorage.getItem('customer_name')&&<UpdateView data={review} onUpdate={handleReviewUpdate} />}
         </div>
     ));
-    
-    
 
-    const noReviews =   <div className="card-review">
-    <div className="card-container-review">
-        <h4 className="item-desc-review-No">No Reviews Having</h4>
+
+
+    const noReviews = <div className="card-review">
+        <div className="card-container-review">
+            <h4 className="item-desc-review-No">No Reviews Having</h4>
+        </div>
     </div>
-</div>
 
     return (
         <div>
--- src/Components/CustomerReview/UpdateView.js
+++ src/Components/CustomerReview/UpdateView.js
@@ -1,25 +1,28 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import './CustomerReviewCSS/review.css'
-import ExisitngCustomerReview from "./ExistingCustomerReview";
+import React, { useState } from "react";
 
-function AddReview() {
+function UpdateView(Prop) {
+    console.log(Prop)
+  
     const [formData, setFormData] = useState({
         Review: "",
         Shop_id: localStorage.getItem('shop_id'),
         Customer_Name: localStorage.getItem('customer_name'),
-        star:0
+        star: 0
     });
     const [nameValue, setnameValue] = useState('')
     const [reviewAdded, setReviewAdded] = useState(false)
     const [selectedOption, setSelectedOption] = useState("Rate this shop");
+    
+     async function handleSubmit(event) {
+        event.preventDefault();
+        console.log('e')
+        await axios.put('http://localhost:4000/create/'+Prop.data._id,formData).then(u=>{
+            console.log(u)
+            Prop.onUpdate();
+        })
 
-    useEffect(() => {
-        if (reviewAdded) {
-            setReviewAdded(false)
-        }
-    }, [reviewAdded])
-
+    }
     function handleInputChange(e) {
         const name = e.target.name;
         const value = e.target.value;
@@ -30,7 +33,6 @@ function AddReview() {
             return { ...prev, [name]: value }
         })
     }
-
     function handleDropdownChange(event) {
         const provinceValue = event.target.value;
         setSelectedOption(provinceValue);
@@ -39,32 +41,15 @@ function AddReview() {
             return { ...prev, star: provinceValue };
         });
     }
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(formData)
-        axios.post('http://localhost:4000/create/review', formData)
-            .then(response => {
-                console.log(response.data);
-                // Handle success response
-                setReviewAdded(true)
-            })
-            .catch(error => {
-                console.log(error);
-                // Handle error response
-            });
-        setnameValue('')
-    }
-
     return (
         <div>
-            <ExisitngCustomerReview key={reviewAdded} />
             <form className='main-review'>
                 <div className='topic-review'>
-                    <h3 className='head-review'>Customer Review</h3>
+                    <h3 className='head-review'>Update Review</h3>
                 </div>
                 <div>
                     <label className='labelHead-review'>Add A Review</label>
-                    <input type="text" name='Review' onChange={handleInputChange} value={nameValue} />
+                    <input type="text" name='Review' onChange={handleInputChange} value={nameValue} placeholder={Prop.data.Review} />
                 </div>
                 <label htmlFor="dropdown" className='labelHead'>Rate this shop</label>
                 <select id="dropdown" value={selectedOption} onChange={handleDropdownChange}>
@@ -77,11 +62,11 @@ function AddReview() {
                     <option value="6">6 star</option>
                 </select>
                 <div className='form--review'>
-                    <button className='Add Station' onClick={handleSubmit}>Add Review</button>
+                    <button className='Add Station' onClick={handleSubmit}>Update Review</button>
                 </div>
             </form>
         </div>
     )
 }
 
-export default AddReview
+export default UpdateView
