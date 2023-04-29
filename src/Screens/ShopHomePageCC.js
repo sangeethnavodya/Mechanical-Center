a83a7a1a4d1c71edabeaa14ea95cd7f38251a500
sangeethnavodya <pererakasn.19@uom.lk>
2023-04-30 01:40:05 +0530

    shop

--- src/Screens/ShopHomePageCC.js
+++ src/Screens/ShopHomePageCC.js
@@ -4,14 +4,13 @@ import ShopList from "../Components/ExistingShop";
 import ShopListCC from "../Components/ExsitingShopCC";
 import FuelAdd from "../Components/FuelAddComponent";
 import Navbar from "../Components/navbar_component";
-import ShopCC from "../Components/Shop/ShopCC";
 
 export default class ShopHomePageCC extends Component {
     render() {
         return (
             <div>
             <Navbar/>
-            <ShopCC/>
+            <ShopListCC/>
             </div>
         )
     }
