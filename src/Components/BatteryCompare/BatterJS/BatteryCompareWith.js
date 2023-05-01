36f4a44af596b2c182bbe3ce7f01cf807cd13bd8
sangeethnavodya <pererakasn.19@uom.lk>
2023-05-01 13:00:39 +0530

    changes

--- src/Components/BatteryCompare/BatterJS/BatteryCompareWith.js
+++ src/Components/BatteryCompare/BatterJS/BatteryCompareWith.js
@@ -7,8 +7,8 @@ function BatteryCompareWith() {
         <div>
             <h2 className="head-battery">Battery Comparision</h2>
             <div style={{ display: "flex", flexDirection: "row" }}>
-                <BatterComparision style={{ flex: 1, width: "200px" }}  />
-                <BatterComparision style={{ flex: 1, width: "200px" }} />
+                <BatterComparision style={{ flex: 1, width: "500px" }}  />
+                <BatterComparision style={{ flex: 1, width: "500px" }} />
             </div>
 
 
