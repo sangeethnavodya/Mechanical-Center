05c1cacad99615165f1bfe27958e78fad788fb25
sangeethnavodya <pererakasn.19@uom.lk>
2023-05-10 00:17:31 +0530

    Ui changes

--- src/Components/navbar_component.js
+++ src/Components/navbar_component.js
@@ -1,17 +1,20 @@
 import React,{Component} from 'react'
 import './ComponentCss/navbar.css'
+import {Button} from "antd";
+import {useNavigate} from "react-router-dom";
 
-export default class Navbar extends Component{
-    render(){
+export default function Navbar(){
+    const navigate=useNavigate();
+    function buttonLog(){
+        navigate('/')
+    }
         return(
             <nav>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Contact</a></li>
+            <ul style={{height:"70px"}}>
+              <Button><a href="#">Home</a></Button>
+              <Button style={{marginLeft:"30px"}} onClick={()=>buttonLog()}>LogOut</Button>
             </ul>
           </nav>
         )
-    }
+
 }
